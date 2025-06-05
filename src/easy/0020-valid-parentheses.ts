d31aea5289fe86f01600ac24f3ee9fb0d14b476a
Muhammad Faisal <muhammad.faisal@ist.id>
2025-06-05 13:08:05 +0700

    feat(easy no 0020): add solution for valid-parentheses

---
+++ src/easy/0020-valid-parentheses.ts
@@ -0,0 +1,26 @@
+function isValid(s: string): boolean {
+  const stack: string[] = [];
+  const mapping= {
+    ')' : '(',
+    ']' : '[', 
+    '}' : '{',
+  }
+
+  for(let char of s) {
+    if(char in mapping) {
+      const top = stack.length > 0 ? stack.pop() : "*"; // mengambil char paling atas dari stack kalo ndak kosong
+
+      // mencocokkan pasangan parentheses
+      if(mapping[char as keyof typeof mapping] !== top) {
+        return false
+      }
+    } else {
+      // jika dia tanda buka maka masukkan ke stack
+      stack.push(char)
+    }
+  }
+
+  return stack.length === 0
+}
+
+console.log(isValid("()[]{}"))
