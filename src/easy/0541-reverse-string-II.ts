764be7b4849fe0c09acb340a9d40e33359b99aa6
Muhammad Faisal <muh.faisal572@gmail.com>
2025-12-09 13:40:30 +0700

    refactor (easy no 0541) : enhance logic

--- src/easy/0541-reverse-string-II.ts
+++ src/easy/0541-reverse-string-II.ts
@@ -2,7 +2,7 @@ function reverseStr(s: string, k: number): string {
   let arr = s.split("");
 
   // loop melali string dgn step 2k
-  for (let i = 0; i < arr.length; i += k * k) {
+  for (let i = 0; i < arr.length; i += 2 * k) {
     const end = Math.min(i + k, arr.length); // pastiin ndak mebdih dr panjang string
     const subArray = arr.slice(i, end).reverse(); //
 
@@ -14,3 +14,19 @@ function reverseStr(s: string, k: number): string {
 
 // console.log(reverseStr("abcdefg", 2));
 console.log(reverseStr("abcd", 2));
+
+// === opsi solusi terbaik dari leetcode ===
+function reverseStrBestSolution(s: string, k: number): string {
+  let arr = s.split("");
+  for (let i = 0; i < s.length; i += 2 * k) {
+    // Reverse the first k characters for every 2k segment
+    let left = i;
+    let right = Math.min(i + k - 1, s.length - 1);
+    while (left < right) {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
+      right--;
+    }
+  }
+  return arr.join("");
+}
